9ca0d12f4571d86e9551ffd72fc23d17997899d3
gitkamyah <kambiz.ghoorchian@gmail.com>
2018-10-30 10:32:45 +0100

    clena repository

--- src/TestClass.java
+++
@@ -1,8 +0,0 @@
-
-public class TestClass {
-
-	public static void main(String[] args){
-		System.out.println("This is the new application");
-		System.out.println("This is the second change to the application.");
-	}
-}
